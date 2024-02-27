6d4c4a142d200cd4e8fe005473f1369c4bc4d648
nixpig <143995476+nixpig@users.noreply.github.com>
2024-02-27 07:36:05 +0000

    feat: add TLS

--- Makefile
+++ Makefile
@@ -1,3 +1,22 @@
+CONFIG_PATH=${HOME}/.proglog/
+
+.PHONY: init
+init:
+	mkdir -p ${CONFIG_PATH}
+
+.PHONY: gencert
+gencert:
+	cfssl gencert \
+		-initca test/ca-csr.json | cfssljson -bare ca
+	cfssl gencert \
+		-ca=ca.pem \
+		-ca-key=ca-key.pem \
+		-config=test/ca-config.json \
+		-profile=server \
+		test/server-csr.json | cfssljson -bare server
+	mv *.pem *.csr ${CONFIG_PATH}
+
+.PHONY: compile
 compile:
 	protoc api/v1/*proto \
 		--go_out="." \
@@ -7,7 +26,6 @@ compile:
 		--proto_path="."
 
 .PHONY: test
-test: export APP_ENV=test
 test: 
 	go test -v -race -buildvcs ./...
 
@@ -16,5 +34,6 @@ test_coverage:
 	go test -v -race -buildvcs -coverprofile=coverage.out ./...
 	go tool cover -html=coverage.out
 
+.PHONY: clean
 clean:
 	rm -rf bin tmp 
--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -7,10 +7,11 @@ import (
 	"testing"
 
 	api "github.com/nixpig/proglog/api/v1"
+	"github.com/nixpig/proglog/internal/config"
 	"github.com/nixpig/proglog/internal/log"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/status"
 )
 
@@ -35,12 +36,33 @@ func setupTest(t *testing.T, fn func(*Config)) (client api.LogClient,
 ) {
 	t.Helper()
 
-	l, err := net.Listen("tcp", ":0")
+	l, err := net.Listen("tcp", "127.0.0.1:0")
 	require.NoError(t, err)
 
-	clientOptions := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
-	cc, err := grpc.Dial(l.Addr().String(), clientOptions...)
+	clientTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+		CAFile: config.CAFile,
+	})
 	require.NoError(t, err)
+
+	clientCreds := credentials.NewTLS(clientTLSConfig)
+
+	cc, err := grpc.Dial(
+		l.Addr().String(),
+		grpc.WithTransportCredentials(clientCreds),
+	)
+	require.NoError(t, err)
+
+	client = api.NewLogClient(cc)
+
+	serverTLSConfig, err := config.SetupTLSConfig(config.TLSConfig{
+		CertFile:      config.ServerCertFile,
+		KeyFile:       config.ServerKeyFile,
+		CAFile:        config.CAFile,
+		ServerAddress: l.Addr().String(),
+	})
+	require.NoError(t, err)
+
+	serverCreds := credentials.NewTLS(serverTLSConfig)
 
 	dir, err := os.MkdirTemp("", "server-test")
 	require.NoError(t, err)
@@ -56,20 +78,17 @@ func setupTest(t *testing.T, fn func(*Config)) (client api.LogClient,
 		fn(cfg)
 	}
 
-	server, err := NewGRPCServer(cfg)
+	server, err := NewGRPCServer(cfg, grpc.Creds(serverCreds))
 	require.NoError(t, err)
 
 	go func() {
 		server.Serve(l)
 	}()
 
-	client = api.NewLogClient(cc)
-
 	return client, cfg, func() {
 		server.Stop()
 		cc.Close()
 		l.Close()
-		clog.Remove()
 	}
 }
 
